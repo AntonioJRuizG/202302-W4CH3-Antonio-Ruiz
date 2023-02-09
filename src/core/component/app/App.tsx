8bd77628041b566a23d3a5c171b8e30499434279
AntonioJesusRG <antoniogar129@gmail.com>
2023-02-09 13:51:34 +0100

    fix eclint error at merge

--- src/core/component/app/App.tsx
+++ src/core/component/app/App.tsx
@@ -4,11 +4,7 @@ import { Card } from "../../../features/cardlist/components/card/card";
 // import "./App.css";
 
 function App() {
-  return (
-    
-      <Card></Card>
-    
-  );
+  return <Card></Card>;
 }
 
 export default App;
